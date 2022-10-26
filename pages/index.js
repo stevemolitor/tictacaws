f2edbc18d73c7c9321e2f24be96b9136cfb84eff
Steve Molitor <steve.molitor@zapier.com>
2022-10-26 17:30:33 -0500

    Add SRR rendering, verify BFF route

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,24 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BFF_URL}/api/hello`);
+  const { name } = await res.json();
+  return { props: { name } };
+};
+
+export default function Home({ name }) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tic Tac AWS</title>
+        <title>Tic Tac AWS, SSR Rendered Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Tic Tac AWS</h1>
+        <h2>Data from API route (BFF):</h2>
+        <div>{name}</div>
       </main>
     </div>
   );
